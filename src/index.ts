35b173165f40dcad6451ecab528d55f7cebf70dd
jake manx <jacobzadelman@gmail.com>
2019-01-22 17:02:04 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+import "reflect-metadata";
+@Resolver()
+class HelloResolver {
+  @Query(() => String, { name: "helloWorld" })
+  async hello() {
+    return "Hello World!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
