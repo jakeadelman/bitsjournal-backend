c2bfc925673776d34ad250ef77bbb768315b7345
jake manx <jacobzadelman@gmail.com>
2019-02-24 03:47:51 -0500

    testing envs

---
+++ ormconfig.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    name: "default",
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    // ssl: { rejectUnauthorized: false },
+    synchronize: true,
+    logging: true,
+    entities: [
+      "src/entity/*.*",
+      "src/entity/instagram/*.*",
+      "src/entity/sentiment/*.*"
+    ]
+  };
+} else if (process.env.NODE_ENV === "development") {
+  module.exports = {
+    name: "default",
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [
+      "src/entity/*.*",
+      "src/entity/instagram/*.*",
+      "src/entity/sentiment/*.*"
+    ]
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -12,26 +12,43 @@ import { RegisterResolver } from "./modules/user/Register";
 import { LoginResolver } from "./modules/user/Login";
 import { MeResolver } from "./modules/user/Me";
 import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
-import { FetchTweetResolver } from "./modules/tweet/getTweets";
-import { AddInstaUserResolver } from "./modules/user/instagram/addUser";
-import { AddSearchTermResolver } from "./modules/tweet/addSearchTerm";
-import { FetchTermResolver } from "./modules/user/fetchTerms";
-import { GetDailyTweet } from "./modules/tweet/tweetByHour";
+// import { FetchTweetResolver } from "./modules/tweet/getTweets";
+// import { AddInstaUserResolver } from "./modules/user/instagram/addUser";
+// import { AddSearchTermResolver } from "./modules/tweet/addSearchTerm";
+// import { FetchTermResolver } from "./modules/user/fetchTerms";
+// import { GetDailyTweet } from "./modules/tweet/tweetByHour";
+const path = require("path");
 
 const main = async () => {
   await createConnection();
+  console.log(process.env.NODE_ENV);
+  // console.log(process.env);
+  if (process.env.NODE_ENV === "production") {
+    console.log("loading production VARS");
+    let thePath = path.join(__dirname, "/../envs/prod/.env");
+    require("dotenv").config({ path: thePath });
+  } else if (process.env.NODE_ENV === "development") {
+    let thePath = path.join(__dirname, "/../envs/dev/.env");
+    require("dotenv").config({
+      path: thePath
+    });
+
+    console.log(thePath);
+    console.log(process.env.DB_HOST);
+    console.log("loading dev VARS");
+  }
 
   const schema = await buildSchema({
     resolvers: [
       RegisterResolver,
       LoginResolver,
       MeResolver,
-      ConfirmUserResolver,
-      FetchTweetResolver,
-      AddInstaUserResolver,
-      AddSearchTermResolver,
-      FetchTermResolver,
-      GetDailyTweet
+      ConfirmUserResolver
+      // FetchTweetResolver,
+      // AddInstaUserResolver,
+      // AddSearchTermResolver,
+      // FetchTermResolver,
+      // GetDailyTweet
     ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
@@ -74,7 +91,7 @@ const main = async () => {
   apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
-    console.log("server started on http://localhost:4000/graphql");
+    console.log(`server started on http://localhost:4000/graphql`);
   });
 };
 
