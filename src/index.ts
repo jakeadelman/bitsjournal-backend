8a2730521379b659ee8c22a2d046c5d96c26065b
jake manx <jacobzadelman@gmail.com>
2019-02-25 15:20:35 -0500

    change cors again

--- src/index.ts
+++ src/index.ts
@@ -50,12 +50,12 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: ["http://localhost:3000", "https://socialslant.io", "*"]
-    })
-  );
+  // app.use(
+  //   cors({
+  //     credentials: true,
+  //     origin: ["http://localhost:3000", "https://socialslant.io", "*"]
+  //   })
+  // );
   app.use(
     session({
       store: new RedisStore({
