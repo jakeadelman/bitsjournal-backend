3f6051b19534b2fa132385316e57ecbe2669aae2
Jake Adelman <jacobzadelman@gmail.com>
2020-05-13 22:45:29 -0400

    remove cached dist

--- dist/modules/bitmex/helpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-const date_fns_1 = require("date-fns");
-exports.MoreThanDate = (date) => typeorm_1.MoreThan(date_fns_1.format(date, "YYYY-MM-DD HH:MM:SS"));
-exports.LessThanDate = (date) => typeorm_1.LessThan(date_fns_1.format(date, "YYYY-MM-DD HH:MM:SS"));
-//# sourceMappingURL=helpers.js.map
