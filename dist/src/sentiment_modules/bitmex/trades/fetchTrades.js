9a1165d206ffe881b56d2f58691636949ee60fe3
Jake Adelman <jacobzadelman@gmail.com>
2020-05-13 22:41:29 -0400

    change gitignore

--- dist/src/sentiment_modules/bitmex/trades/fetchTrades.js
+++
@@ -1,7 +0,0 @@
-var cron = require("node-cron");
-console.log("started");
-cron.schedule("*/5 * * * *", () => {
-    console.log("running every minute 3,5");
-    console.log(new Date());
-});
-//# sourceMappingURL=fetchTrades.js.map
--- dist/src/sentiment_modules/timestamps/test-createHours.js
+++
@@ -1,4 +0,0 @@
-const dateFormat = require("dateformat");
-let theHour = dateFormat(new Date(), "yymmddHH");
-console.log(theHour);
-//# sourceMappingURL=test-createHours.js.map
